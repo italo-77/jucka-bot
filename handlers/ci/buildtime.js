932cfcd9a1228bd060ba5d1b99e9b855d894890c
italo-77 <my.secret.6459@gmail.com>
2025-06-19 02:32:55 -0300

    resolvendo const duplicada

--- handlers/ci/buildtime.js
+++ handlers/ci/buildtime.js
@@ -1,9 +1,7 @@
 const github = require('../../services/github');
 
-const github = require('../../services/github');
-
 module.exports = (bot) => {
   bot.command('pullrequests', github.pullRequests);
+  bot.command('buildtime', github.buildTime);
+  bot.command('status', github.statusWorkflow);
 };
-bot.command('buildtime', github.buildTime);
-bot.command('status', github.statusWorkflow);
